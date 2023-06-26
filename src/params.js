b48f9ef4c931aa8ddc0faa5527f26ffa783f9214
Pieter Wigboldus <w3n@w3news.org>
2023-06-26 20:50:38 +0200

    Fallback to default values

--- src/params.js
+++ src/params.js
@@ -9,12 +9,17 @@ import { types } from './types.js'
  */
 export const parseParams = ({ query, spec }) =>
   spec.map(parameter => {
-    const { name, schema } = parameter
-    const { type } = schema
+    const { name, schema, example: exampleValue } = parameter
+    const { type, default: defaultValue } = schema
     const Type = types[type]
-    const paramName = query[name]
-    const value = new Type(paramName).valueOf()
-    return { name, value }
+    const paramName = query?.[name]
+
+    if (paramName) {
+      const value = new Type(paramName).valueOf()
+      return { name, value }
+    }
+
+    return { name, value: defaultValue ?? exampleValue }
   })
     .reduce((acc, { name, value }) => {
       acc[name] = value
--- src/params.test.js
+++ src/params.test.js
@@ -15,6 +15,103 @@ const TestCases = [
         schema: {
           type: 'integer',
           minimum: 1,
+          maximum: 10000,
+          default: 42
+        }
+      },
+      {
+        name: 'page',
+        required: false,
+        in: 'query',
+        example: 0,
+        schema: {
+          type: 'integer',
+          minimum: 0,
+          default: 1
+        }
+      }
+    ],
+    expectedResult: {
+      page: 0,
+      size: 10
+    }
+  },
+  {
+    description: 'Get the default values from the schema if no query params are given',
+    query: {},
+    spec: [
+      {
+        name: 'size',
+        required: false,
+        in: 'query',
+        example: 10,
+        schema: {
+          type: 'integer',
+          minimum: 1,
+          maximum: 10000,
+          default: 42
+        }
+      },
+      {
+        name: 'page',
+        required: false,
+        in: 'query',
+        example: 0,
+        schema: {
+          type: 'integer',
+          minimum: 0,
+          default: 1
+        }
+      }
+    ],
+    expectedResult: {
+      page: 1,
+      size: 42
+    }
+  },
+  {
+    description: 'Get the example values from the schema if no query params are given',
+    query: {},
+    spec: [
+      {
+        name: 'size',
+        required: false,
+        in: 'query',
+        example: 10,
+        schema: {
+          type: 'integer',
+          minimum: 1,
+          maximum: 10000
+        }
+      },
+      {
+        name: 'page',
+        required: false,
+        in: 'query',
+        example: 0,
+        schema: {
+          type: 'integer',
+          minimum: 0
+        }
+      }
+    ],
+    expectedResult: {
+      page: 0,
+      size: 10
+    }
+  },
+  {
+    description: 'It should not throw if no query params are given',
+    query: undefined,
+    spec: [
+      {
+        name: 'size',
+        required: false,
+        in: 'query',
+        example: 10,
+        schema: {
+          type: 'integer',
+          minimum: 1,
           maximum: 10000
         }
       },
