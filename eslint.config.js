fa9921d0097b5dc8b53b6d2d042ec56f8ed43d0c
Pieter Wigboldus <pieter.wigboldus@zilliz.nl>
2025-01-30 22:50:53 +0100

    Fix eslint config file

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,29 @@
 import globals from 'globals'
 import pluginJs from '@eslint/js'
-import { plugins, rules} from '@trojs/lint'
+import { plugins, rules } from '@trojs/lint'
 
 export default [
-    { languageOptions: { globals: globals.node } },
-    pluginJs.configs.recommended,
-    {
-        languageOptions: {
-            ecmaVersion: 'latest',
-            sourceType: 'module'
-        },
-        settings: {
-            jsdoc: {
-                mode: 'typescript'
-            },
-            react: {
-                version: '18.3.1'
-            }
-        },
-        plugins: {
-            ...plugins
-        },
-        rules: {
-            ...rules.all
-        },
-        files: ['src/**/*.js']
-    }
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module'
+    },
+    settings: {
+      jsdoc: {
+        mode: 'typescript'
+      },
+      react: {
+        version: '18.3.1'
+      }
+    },
+    plugins: {
+      ...plugins
+    },
+    rules: {
+      ...rules.all
+    },
+    files: ['**/*.js']
+  }
 ]
